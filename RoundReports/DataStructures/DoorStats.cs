91c8da701c9ca8b580d45af45662dd8d8c7513b4
Thunder <thunder@scpetgame.com>
2022-06-28 18:53:40 -0400

    wip

---
+++ RoundReports/DataStructures/DoorStats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundReports
+{
+    public class DoorStats : IReportStat
+    {
+        public string Title => "Door Statistics";
+        public int Order => 8;
+        public int DoorsOpened { get; set; }
+        public int DoorsClosed { get; set; }
+        public int DoorsDestroyed { get; set; }
+    }
+}
