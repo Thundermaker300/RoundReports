15f9b18eff4b0f4df09748a3794c9bf902731d66
Thunder <thunder@scpetgame.com>
2022-06-28 00:27:27 -0400

    Initial commit

---
+++ RoundReports/MainPlugin.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Interfaces;
+using Exiled.API.Features;
+using System.ComponentModel;
+
+namespace RoundReports
+{
+    public class MainPlugin : Plugin<Config>
+    {
+    }
+
+    public class Config : IConfig
+    {
+        [Description("Whether or not the plugin is active.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Provide a Discord webhook to send reports to.")]
+        public string DiscordWebhook { get; set; } = string.Empty;
+    }
+}
