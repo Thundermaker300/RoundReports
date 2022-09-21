9fb80946cf4e77804302b965042f0023779095a0
Thunder <thundermaker300@gmail.com>
2022-09-20 21:07:49 -0400

    Separate config file and main file

---
+++ RoundReports/Config.cs
@@ -0,0 +1,35 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+using EBroadcast = Exiled.API.Features.Broadcast;
+
+namespace RoundReports
+{
+    public class Config : IConfig
+    {
+        [Description("Whether or not the plugin is active.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Your Paste.ee key. Get this from https://paste.ee/account/api after creating a paste.ee account. The plugin cannot function without a valid Pastee key!")]
+        public string PasteKey { get; set; } = "";
+
+        [Description("Time until reports expire. Valid values: Never, 1D, 7D, 14D, 1M, 3M, 6M")]
+        public string ExpiryTime { get; set; } = "1M";
+
+        [Description("Provide a Discord webhook to send reports to.")]
+        public string DiscordWebhook { get; set; } = string.Empty;
+        [Description("Send report links in server console when compiled?")]
+        public bool SendInConsole { get; set; } = true;
+        [Description("Broadcast to show at the end of the round.")]
+        public EBroadcast EndingBroadcast { get; set; } = new EBroadcast("View the end-of-round report on our Discord server!");
+
+        [Description("Name of the server, without any formatting tags, as it will be shown in the report.")]
+        public string ServerName { get; set; } = string.Empty;
+
+        [Description("Determines the footer text, as shown on the Discord embed. Valid arguments: {PLAYERCOUNT}, {ROUNDTIME}, {TOTALKILLS}, {TOTALDEATHS}")]
+        public string FooterText { get; set; } = "{PLAYERCOUNT} players connected";
+
+        [Description("Determines the format of timestamps.")]
+        public string FullTimeFormat { get; set; } = "MMMM dd, yyyy hh:mm:ss tt";
+        public string ShortTimeFormat { get; set; } = "HH:mm:ss";
+    }
+}
