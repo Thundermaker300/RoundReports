d7f759247ae32d59e53685ea66de1859544122f3
Thunder <thundermaker300@gmail.com>
2022-10-06 13:39:06 -0400

    Keep for testing later

---
+++ RoundReports/Services/ChartGen.cs
@@ -0,0 +1,43 @@
+﻿using Exiled.API.Features;
+using Newtonsoft.Json;
+using QuickChart;
+using System.Linq;
+
+namespace RoundReports
+{
+    public class ChartGen
+    {
+        public Reporter Reporter { get; }
+        public Chart Chart { get; }
+        public GraphSettings Settings { get; }
+
+        public ChartGen(Reporter reporter)
+        {
+            Chart = new();
+            Settings = new() { Data = new() { DataSets = new()  } };
+            Reporter = reporter;
+        }
+
+        public string GetUrl()
+        {
+            Chart.Config = JsonConvert.SerializeObject(Settings);
+            return Chart.GetUrl();
+        }
+
+        public void Fill(string type)
+        {
+            if (type is "kills")
+            {
+                OrganizedKillsStats stats = Reporter.GetStat<OrganizedKillsStats>();
+                Settings.Type = "bar";
+                Settings.Data.Labels = stats.KillsByPlayer.Keys.Select(player => Reporter.GetDisplay(player)).ToArray();
+                foreach (var data in stats.KillsByPlayer)
+                {
+                    Settings.Data.DataSets.Add(new() { Label = Reporter.GetDisplay(data.Key), Data = new[] { (float)data.Value } });
+                }
+            }
+        }
+
+
+    }
+}
---
+++ RoundReports/Structures/GraphSettings.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace RoundReports
+{
+    public class GraphData
+    {
+        [JsonProperty("labels")]
+        public string[] Labels { get; set; }
+
+        [JsonProperty("datasets")]
+        public List<GraphDataSet> DataSets { get; set; }
+    }
+
+    public class GraphDataSet
+    {
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("data")]
+        public float[] Data { get; set; }
+    }
+
+    public class GraphSettings
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("data")]
+        public GraphData Data { get; set; }
+    }
+}
