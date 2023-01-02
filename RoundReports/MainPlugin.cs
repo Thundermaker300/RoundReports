e19f992cf0cc1b5a227e5c86edc2d3754eb50da5
Thunder <thundermaker300@gmail.com>
2023-01-02 12:23:13 -0500

    Version bump

--- RoundReports/MainPlugin.cs
+++ RoundReports/MainPlugin.cs
@@ -21,7 +21,7 @@ namespace RoundReports
     {
         public override string Name => "RoundReports";
         public override string Author => "Thunder";
-        public override Version Version => new(0, 5, 3);
+        public override Version Version => new(0, 5, 4);
         public override Version RequiredExiledVersion => new(6, 0, 0);
         public override PluginPriority Priority => PluginPriority.Last;
 
