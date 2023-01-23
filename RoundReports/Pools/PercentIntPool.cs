3baf4be3f507e0ad40ed7acefcef6ef764326790
Thunder <thundermaker300@gmail.com>
2023-01-23 00:00:48 -0500

    doc

--- RoundReports/Pools/PercentIntPool.cs
+++ RoundReports/Pools/PercentIntPool.cs
@@ -5,12 +5,22 @@
 
     using Exiled.API.Features.Pools;
 
+    /// <summary>
+    /// Pool for storing <see cref="PercentInt"/> objects.
+    /// </summary>
     public class PercentIntPool : IPool<PercentInt>
     {
         private readonly ConcurrentQueue<PercentInt> pool = new();
 
+        /// <summary>
+        /// Gets the static <see cref="PercentIntPool"/> object.
+        /// </summary>
         public static PercentIntPool Pool { get; } = new();
 
+        /// <summary>
+        /// Gets a <see cref="PercentInt"/>.
+        /// </summary>
+        /// <returns>A <see cref="PercentInt"/>.</returns>
         public PercentInt Get()
         {
             if (!pool.TryDequeue(out PercentInt result))
