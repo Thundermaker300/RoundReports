e18034ecfb2fcfb8c62ddab217b319a903e8b008
Thunder <thundermaker300@gmail.com>
2023-03-10 08:37:04 -0500

    Fix build issue

--- RoundReports/Patches/TeslaShocksPatch.cs
+++ RoundReports/Patches/TeslaShocksPatch.cs
@@ -11,7 +11,14 @@
     [HarmonyPatch(typeof(TeslaGate), nameof(TeslaGate.ServerSideCode))]
     public static class TeslaShocksPatch
     {
+        /// <summary>
+        /// Prefix patch.
+        /// </summary>
+        /// <param name="__instance">The <see cref="TeslaGate"/> instance.</param>
+        /// <returns>Whether or not to execute the original method - always true.</returns>
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter.
         public static bool Prefix(TeslaGate __instance)
+#pragma warning restore SA1313
         {
             if (!__instance.InProgress && Round.InProgress)
             {
