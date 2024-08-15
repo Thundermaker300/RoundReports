f605c51e31170acd0967da0266ba95d12fee3750
AlexInABox <main@alexinabox.de>
2024-08-15 15:22:13 +0200

    add flag to send @silent notifications

--- RoundReports/Reporter.cs
+++ RoundReports/Reporter.cs
@@ -613,6 +613,7 @@
                                     },
                                 },
                             },
+                            Flags = [ 4096 ],
                         };
 
                         string hookDataString = JsonConvert.SerializeObject(hookData);
